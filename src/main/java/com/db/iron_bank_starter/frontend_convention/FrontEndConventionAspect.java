45bec9c376963dcc826c404c22abff0519de8645
Anastasiia Turenko <aaturenko@gmail.com>
2018-09-04 13:43:39 +0300

    fix frond end convention advice

--- src/main/java/com/db/iron_bank_starter/frontend_convention/FrontEndConventionAspect.java
+++ src/main/java/com/db/iron_bank_starter/frontend_convention/FrontEndConventionAspect.java
@@ -4,8 +4,10 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+@ControllerAdvice
 public class FrontEndConventionAspect implements ResponseBodyAdvice {
     @Override
     public boolean supports(MethodParameter methodParameter, Class aClass) {
